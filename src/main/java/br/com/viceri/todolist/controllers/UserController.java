4e4c06238e0db1d6a5546098495574bb89bcab01
Vanessa Guimaraes <vanessaguima@outlook.com>
2022-02-23 20:03:05 -0300

    feat: add auth controller

--- src/main/java/br/com/viceri/todolist/controllers/UserController.java
+++ src/main/java/br/com/viceri/todolist/controllers/UserController.java
@@ -1,9 +1,13 @@
 package br.com.viceri.todolist.controllers;
 
 import br.com.viceri.todolist.dto.CreateUserDTO;
-import br.com.viceri.todolist.dto.UserDTO;
 import br.com.viceri.todolist.entity.User;
 import br.com.viceri.todolist.services.UserService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +17,32 @@ import java.util.List;
 @RestController()
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@SecurityRequirement(name="to-do-list")
 public class UserController {
 
     private final UserService userService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retrieve all users information successfully.", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized.", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error.",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     @GetMapping()
-    public ResponseEntity<List<User>> getUser(){
+    public ResponseEntity<List<User>> getUsers(){
         List<User> users = this.userService.getAll();
         return ResponseEntity.ok(users);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created user successfully.", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized.", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error.",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     @PostMapping()
-    public ResponseEntity<CreateUserDTO> createUser(@RequestBody() CreateUserDTO createUserDTO){
-        this.userService.create(createUserDTO);
-        return ResponseEntity.ok(createUserDTO);
+    public ResponseEntity<User> createUser(@RequestBody() CreateUserDTO createUserDTO){
+        final User user = this.userService.create(createUserDTO);
+        return ResponseEntity.created(null).body(user);
     }
 }
